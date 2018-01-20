b60296397269db3ddd378d7cd77c8d6121fcca22
Sionae <samcopios@gmail.com>
2018-01-20 23:23:13 +0100

    Add files via upload

--- PrimeMain.py
+++ PrimeMain.py
@@ -1,7 +1,7 @@
 from PrimeLoop import *
 import os
 
-os.chdir("C:/Users/samco/Desktop/Code/Maths/PrimeFinder")
+os.chdir("C:\\Users\\samco\\Desktop\\Code\\Python\\PrimeCalc")
 
 def l2str(l):
     s = ""
@@ -10,6 +10,15 @@ def l2str(l):
         s += str(elt) + " "
 
     return s
+
+def delAncient(l, lengthi):
+    count = 0
+    while count < lengthi:
+        del l[0]
+        count += 1
+
+    return l
+
 
 def str2l(s):
     """Split returning integers"""
@@ -21,22 +30,30 @@ def str2l(s):
 
     return splitedList
 
+
 with open("primes.txt", "r+") as f:
     data = f.readlines()
 
     if len(data) == 0:
         primelist = [2, 3, 5, 7, 9, 11, 13]
-        data = [""]
+        data.append("")
 
     else:
         primelist = str2l(data[0])
 
-    primelist = PrimeLoop(primelist, 100000)
+
+
+    lengthi = len(primelist)
+
+    primelist = PrimeLoop(primelist, 1000)
+    if lengthi != 7:
+        primelist = delAncient(primelist, lengthi)
     data[0] = l2str(primelist)
     f.writelines(data)
 
     print("Done")
     print("Total number of prime numbers : {}".format(len(primelist)))
+    print("Highest prime number : {}".format(primelist[-1]))
 
 #Security closing
 f.close()
